51e17ab5bf91736daf7642dd37c90bfe40d52137
ucesccf <ucesccf@ucl.ac.uk>
2022-01-05 17:26:45 +0000

    Add: Datacleaning file & basic data pre-processing

--- 1_Data_Cleaning.R
+++ 1_Data_Cleaning.R
@@ -1,7 +1,8 @@
-# CRIME SPATIAL ANALYSIS
+# Project: CRIME SPATIAL ANALYSIS
+# File: DATA CLEANING
 # Module: CEGE0097
-# Student no.: 20198829 ...
-# Date: 22 January 2022
+# Student no.: 20198829, 20164326 ...
+# Date: 22 JANUARY 2022
 
 # To run this code, set the work directory to folder containing the provided data.
 setwd('C:/Users/offne/OneDrive - University College London/CEGE0097 Spatial Analysis and Geocomputation/Coursework')
@@ -16,50 +17,61 @@ library(leaflet)
 library(spatialEco)
 library(dplyr)
 
-#### 1. LOAD DATA ####
+# NOTE: Make sure all of your necessary pre-processing is done here (not in 2_Non_Spatial_EDA)
+
+#### 1.1 LOAD DATA ####
 # Open csv files
 ss <- read.csv(file='Data/2016-06-metropolitan-stop-and-search.csv', fileEncoding="UTF-8-BOM")
 crime <- read.csv(file='Data/2016-06-metropolitan-street.csv', fileEncoding="UTF-8-BOM")
 pp <- read.csv(file='Data/2016-police-perceptions.csv', fileEncoding="UTF-8-BOM")
+pp_wab <- read.csv(file='Data/2015-16 _to_2020-21_inclusive_neighbourhood_indicators_final_221221.csv') # wab = white, asian, black
 
 # Open shape files
-proj <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
-borough <- readOGR(dsn="Data/London_Shapefiles/London_Borough_Excluding_MHW.shp")
-borough <- spTransform(borough, CRS(proj))
-names(borough@data)[1] <- "DISTRICT"
+proj <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0" #set projections
 ward <- readOGR(dsn="Data/London_Shapefiles/London_Ward.shp")
 ward <- spTransform(ward, CRS(proj))
+borough <- readOGR(dsn="Data/London_Shapefiles/London_Borough_Excluding_MHW.shp")
+borough <- spTransform(borough, CRS(proj))
+names(borough@data)[1] <- "DISTRICT" # change name of spatial delineation (col1) to match ward
 
+#### 1.2 GENERAL DATA CLEANING & AGGREGATION ####
 
-#### 2. CLEAN & AGGREGATE DATA ####
-# Check missing values
+### Check for missing values
 sapply(crime, function(x) sum(is.na(x)))
 sapply(ss, function(x) sum(is.na(x)))
 sapply(pp, function(x) sum(is.na(x)))
+sapply(pp_wab, function(x) sum(is.na(x)))
 sapply(borough@data, function(x) sum(is.na(x)))
 sapply(ward@data, function(x) sum(is.na(x)))
 
-# Drop Columns where all instances are miSsing OR irrelevant 
+### create subsets and drop missing/irrelevant fields
 crime <- subset (crime, select = -c(Crime.ID, Context, Reported.by, Falls.within, Location, LSOA.code, LSOA.name, Last.outcome.category))
-ss <- subset (ss, select = -c(Part.of.a.policing.operation, Type, Legislation, Policing.operation, Removal.of.more.than.just.outer.clothing, Outcome.linked.to.object.of.search))
-pp <- subset (pp, select = -c(Code, Group, Total.Notifiable.Offences.rate))
-borough@data <- subset (borough@data, select = -c(SUB_2006, SUB_2009))
+ss <- subset(ss, select = -c(Part.of.a.policing.operation, Type, Legislation, Policing.operation, Removal.of.more.than.just.outer.clothing, Outcome.linked.to.object.of.search))
+borough@data <- subset(borough@data, select = -c(SUB_2006, SUB_2009))
+pp <- subset(pp, select = -c(2:3,5:6,9:22)) # selects only good-job, fair and listens indicators
+pp <- pp[ , c(1, 3, 4, 2)] # reorder to neighbour, fair, listens, good-job
 
-# Drop Values with NA (831 in lat/long)
+### Drop Values with NA (831 in lat/long)
 crime <- na.omit(crime)
 ss <- na.omit(ss)
+pp <- na.omit(pp)
+pp_wab <- na.omit(pp_wab)
 
-# Add unique id's
+### Add unique id's
 ss$ID <- seq_along(ss[,1])
 crime$ID <- seq_along(crime[,1])
 
-# Change data types where applicable
-pp <- pp[-1,]
-names <- colnames(pp)[-1]
-pp[ ,names] <- apply(pp[ , names], 2, function(x) as.numeric(as.character(x))) # Change characters to numeric
 
 
+#### 1.2a Police Perceptions #### 
 
-#### 2.a #### 
-#### 2.b #### 
-#### 2.c #### 
+# TOMMY: Add your PRE-PROCESSING bits here
+
+#### 1.2b Stop and Search #### 
+
+# DANNI: Add your PRE-PROCESSING bits here
+
+#### 1.2c Crime #### 
+
+# PRATIBHA: Add your PRE-PROCESSING bits here
+
