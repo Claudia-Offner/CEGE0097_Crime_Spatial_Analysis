a83c7b684ceb3aee8da09397ee18b63aaf795d9c
ucesccf <ucesccf@ucl.ac.uk>
2022-01-08 14:10:43 +0000

    Update: Transform PP lists to spatial data nd combine df's

--- 4_Spatial_Regression.R
+++ 4_Spatial_Regression.R
@@ -5,50 +5,62 @@
 # Date: 22 JANUARY 2022
 
 # To run this code, set the work directory to folder containing the provided files & data
-setwd('C:/Users/offne/Documents/crime_spatial_analysis/')
+setwd('C:/Users/offne/Documents/GitHub/CEGE0097_Crime_Spatial_Analysis')
 
-#### 2.1 LOAD CLEANED DATA ####
+# CLAUDIA COMENTS:
+# What is fairSS for PP_borough race datasets?
+# (NEED i.e.: ss_ward2_black, ss_ward2_asian, ss_ward2_white, ss_ward2_male, ss_ward2_female)
+# (NEED i.e.:  crime_ward_antiBeh; crime_ward_offense; crime_ward_allTheft)
+
+#### 4.1 LOAD CLEANED DATA ####
 suppressWarnings(source("1_Data_cleaning.R"))
 
 # Remove all environment objects except those of interest to this analysis
-# rm(list=ls()[! ls() %in% c("borough", "ward", "proj")])
+rm(list=ls()[! ls() %in% c('crime_ward', 
+                           'ss_ward2',  
+                           'pp_borough', 'pp_black_borough', 'pp_asian_borough', 'pp_white_borough', 
+                           'borough', 'ward', 'proj')])
+
+# (NEED i.e.: ss_ward2_black, ss_ward2_asian, ss_ward2_white, ss_ward2_male, ss_ward2_female)
+# (NEED i.e.:  crime_ward_antiBeh; crime_ward_offense; crime_ward_allTheft)
+
+# Make PP lists Spatial objects
+pp_borough <- pp_borough[[1]]
+pp_black_borough <- pp_black_borough[[1]]
+pp_asian_borough <- pp_asian_borough[[1]]
+pp_white_borough <- pp_white_borough[[1]]
+pp_borough <- merge(borough, pp_borough[ , c(1,6:ncol(pp_borough))], by='DISTRICT')
+pp_black_borough <- merge(borough, pp_black_borough[ , c(1,6:ncol(pp_black_borough))], by='DISTRICT')
+pp_asian_borough <- merge(borough, pp_asian_borough[ , c(1,6:ncol(pp_asian_borough))], by='DISTRICT')
+pp_white_borough <- merge(borough, pp_white_borough[ , c(1,6:ncol(pp_white_borough))], by='DISTRICT')
 
 
-#### Combine Data files ####
 
-### combine pp from all ethnicities 
-pp_per_borough <- merge(borough, pp_ag, by='DISTRICT')
-pp_per_borough <- merge(pp_per_borough, pp_white_ag, by='DISTRICT')
-pp_per_borough <- merge(pp_per_borough, pp_asian_ag, by='DISTRICT')
-pp_per_borough <- merge(pp_per_borough, pp_black_ag, by='DISTRICT')
-pp_per_borough <- pp_per_borough[order(pp_per_borough$DISTRICT),]
-view(pp_per_borough)
+#### 4.2 MERGE DATASETS ####
 
-### combine data (TT: not sure what this df is for)
-df <- merge(crime_ward, ss_ward, by='NAME')
-df <- merge(df, pp_mean, by='DISTRICT')
-df <- merge(df, pp_white_mean, by='DISTRICT')
-df <- merge(df, pp_asian_mean, by='DISTRICT')
-df <- merge(df, pp_black_mean, by='DISTRICT')
+# POLICE PERCEPTION: Merge variables of Interest (4) 
+### goodjob, fair, listens, mean by ALL & race (black, asian, white)
+### What is fairSS for PP_borough race datasets?
 
-### Add pp_mean to ss dataset
-crime <- merge(crime, pp_mean, by='DISTRICT')
-crime <- merge(crime, pp_white_mean, by='DISTRICT')
-crime <- merge(crime, pp_asian_mean, by='DISTRICT')
-crime <- merge(crime, pp_black_mean, by='DISTRICT')
-view(crime)
+pp_df <- merge(pp_borough, pp_black_borough[ , c(1,6:ncol(pp_black_borough))], by='DISTRICT')
+pp_df <- merge(pp_df, pp_asian_borough[ , c(1,6:ncol(pp_asian_borough))], by='DISTRICT')
+pp_df <- merge(pp_df, pp_white_borough[ , c(1,6:ncol(pp_white_borough))], by='DISTRICT')
 
-### Add pp_mean to ss dataset
-ss <- merge(ss, pp_mean, by='DISTRICT')
-ss <- merge(ss, pp_white_mean, by='DISTRICT')
-ss <- merge(ss, pp_asian_mean, by='DISTRICT')
-ss <- merge(ss, pp_black_mean, by='DISTRICT')
-view(ss)
 
-### drop ss/crime points where no pp data
-sapply(crime@data, function(x) sum(is.na(x)))
-sapply(ss@data, function(x) sum(is.na(x)))
-sapply(pp_per_borough@data, function(x) sum(is.na(x)))
-crime@data <- na.omit(crime@data)
-ss@data <- na.omit(ss@data)
-pp_per_borough@data <- na.omit(pp_per_borough@data)
+# STOP & SEARCH: Merge variables of Interest (6)
+### ss_occurance by ALL, race (black, asian, white) and gender (male, female)
+
+ss_df <- ss_ward2
+
+# CRIME: Variables of Interest (4)
+### crime_occurance by ALL, type (anti-social behaviour, violence & sexual offense, all theft)
+
+crime_df <- crime_ward
+
+# COMBINE ALL DATASETS: For regression analysis
+reg_df <- merge(ss_df, crime_df, by='NAME')
+reg_df <- merge(reg_df, pp_df[ , c(1,6:ncol(pp_df))], by='DISTRICT')
+
+
+
+
